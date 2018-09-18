7a0d8c65d389a7ca49274f05b55251e94411f7a3
Umar Mash <mafulun4real@gmail.com>
2018-09-18 18:50:27 +0100

    Changed name to Nairation

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 		<nav className="nav-wrapper grey darken-3">
 			<div className="container">
 				<Link to="/" className="brand-logo">
-					Mario Plan
+					Nairation
 				</Link>
 				<SignedInLinks />
 				<SignedOutLinks />
